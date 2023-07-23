91ee43b104fad22751ce598142f1ecf9e710d212
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-07-24 02:09:52 +0300

    Create 1-insertion_sort_list.c

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - function that sorts a doubly linked list
+ * of integers in ascending order using the Insertion sort algorithm
+ *
+ * @list: list to sort.
+ *
+ * Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+	(void)list;
+}
