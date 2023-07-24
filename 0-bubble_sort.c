c8897b628a7843a5c6359172c0ffc223a365d4af
Mohamed Tharwat <mohamed.sarwat.000@gmail.com>
2023-07-24 13:06:49 +0300

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -17,10 +17,10 @@ void bubble_sort(int *array, size_t size)
 	if (!array || size < 2)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		sorted = 1;
-		for (j = 0; j < size - i; j++)
+		for (j = 0; j < (size - 1) - i; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
